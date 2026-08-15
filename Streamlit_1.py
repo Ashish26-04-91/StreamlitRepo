4388692bd8e5a13d1c7947b0480e2e97c025f362
Ashish26-04-91 <rawatashishcool@gmail.com>
2026-08-15 17:47:09 +0530

    Fix variable name in Streamlit app output

--- Streamlit_1.py
+++ Streamlit_1.py
@@ -2,4 +2,4 @@ import streamlit as st
 st.title("Hello, Mamta!")
 st.title("Gudde ko lekr mandir chalna hai kya aaj.")
 select_time = st.selectbox("Please select your time",["6 pm","7 pm","8 pm"])
-st.write("okay be ready at", select)
+st.write("okay be ready at", select_time)
