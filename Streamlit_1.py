04c38f1dd1b611e102485db9cf5bebe26ec7cd8f
Ashish26-04-91 <rawatashishcool@gmail.com>
2026-08-15 17:46:45 +0530

    Fix selectbox prompt and improve text formatting

--- Streamlit_1.py
+++ Streamlit_1.py
@@ -1,5 +1,5 @@
 import streamlit as st
 st.title("Hello, Mamta!")
-st.title("Gudde ko lekr mandir chalna hai kya aaj. Please select your time")
-select_time = st.selectbox("6 pm","7 pm","8 pm")
+st.title("Gudde ko lekr mandir chalna hai kya aaj.")
+select_time = st.selectbox("Please select your time",["6 pm","7 pm","8 pm"])
 st.write("okay be ready at", select)
