04670a684754f8294252f5310b437ecc6f1b10bb
Ashish26-04-91 <rawatashishcool@gmail.com>
2026-08-15 17:49:09 +0530

    Enhance time selection with default option and condition
    Added a default option to the time selection box and conditional output.

--- Streamlit_1.py
+++ Streamlit_1.py
@@ -1,5 +1,6 @@
 import streamlit as st
 st.title("Hello, Mamta!")
 st.title("Gudde ko lekr mandir chalna hai kya aaj.")
-select_time = st.selectbox("Please select your time",["6 pm","7 pm","8 pm"])
-st.write("okay be ready at", select_time)
+select_time = st.selectbox("Please select your time",["---Select time---","6 pm","7 pm","8 pm"])
+if select_time != "---Select time---":
+  st.write("okay be ready at", select_time)
